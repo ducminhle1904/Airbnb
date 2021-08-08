6ff9c700d3d9312eabd66df95ed33583a74e5a6f
Minh Đức <ducle7144@gmail.com>
2021-08-08 21:24:22 +0700

    fix ui

--- components/Banner.js
+++ components/Banner.js
@@ -2,17 +2,24 @@ import Image from "next/image";
 
 function Banner() {
   return (
-    <div className="relative h-[300px] sm:h-[400px] lg:h-[500px] xl:h-[600px] 2xl:h-[700px]">
-      <Image
-        src="https://links.papareact.com/0fm"
-        layout="fill"
-        objectFit="cover"
-      />
-      <div className="absolute top-1/2 w-full text-center">
-        <p className="text-sm sm:text-4xl font-bold ">
-          Olympian & Paralympian Online Experience
+    <div
+      className="relative h-[300px] sm:h-[400px] lg:h-[500px] xl:h-[600px] 2xl:h-[700px]"
+      style={{
+        backgroundImage:
+          "url(" +
+          "https://a0.muscache.com/im/pictures/e4a2a61c-589f-4e49-b3b8-968a6bc23389.jpg" +
+          ")",
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        backgroundAttachment: "fixed",
+        backgroundPosition: "center",
+      }}
+    >
+      <div className="absolute top-1/4 w-full text-left ml-10">
+        <p className="text-sm sm:text-6xl font-bold text-white">
+          Olympian & <br /> Paralympian <br /> Online <br /> Experience
         </p>
-        <button className="bg-white px-10 py-4 shadow-md rounded-full font-bold my-3 hover:shadow-xl active:scale-90 transition duration-150">
+        <button className="bg-white px-3 py-2 shadow-md rounded-xl font-bold my-3 hover:shadow-xl active:scale-90 transition duration-150">
           Explore Now{" "}
         </button>
       </div>
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,27 @@
 import Head from "next/head";
 import Image from "next/image";
+import useInView from "react-cool-inview";
+import dynamic from "next/dynamic";
 import Header from "../components/Header";
 import Banner from "../components/Banner";
 import SmallCard from "../components/SmallCard";
-import MediumCard from "../components/MediumCard";
-import LargeCard from "../components/LargeCard";
+const MediumCard = dynamic(() => import("../components/MediumCard"));
+const LargeCard = dynamic(() => import("../components/LargeCard"));
 import Footer from "../components/Footer";
 
 export default function Home({ exploreData, cardData }) {
-  console.log(exploreData);
+  const { observe, inView } = useInView({
+    onEnter: ({ unobserve }) => unobserve(), // only run once
+  });
   return (
-    <div className="">
+    <div className="overscroll-x-none">
       <Head>
         <title>AirBnb</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
       <Banner />
-      <main className="max-w-7xl mx-auto px-8 sm:px-16">
+      <main className="max-w-7xl mx-auto px-8 sm:px-16 ">
         <section className="pt-6">
           <h2 className="text-4xl font-semibold">Explore nearby</h2>
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
@@ -31,7 +35,7 @@ export default function Home({ exploreData, cardData }) {
             ))}
           </div>
         </section>
-        <section className="relative h-[200px] sm:h-[300px] lg:h-[400px] xl:h-[500px] 2xl:h-[600px] rounded-xl mt-5 overflow-hidden">
+        <section className="relative h-96 min-w-[300px] rounded-xl mt-5 overflow-hidden">
           <Image
             src="https://a0.muscache.com/im/pictures/a915ff27-6062-436d-a7a9-007685423f7b.jpg?im_w=720"
             layout="fill"
@@ -46,7 +50,7 @@ export default function Home({ exploreData, cardData }) {
             </button>
           </div>
         </section>
-        <section>
+        <section ref={observe}>
           <h2 className="text-4xl font-semibold py-8">Live Anywhere</h2>
           <div className="flex space-x-3 overflow-scroll scrollbar-hide p-3 -ml-3">
             {cardData.map((item, index) => (
@@ -54,13 +58,14 @@ export default function Home({ exploreData, cardData }) {
             ))}
           </div>
         </section>
-
-        <LargeCard
-          img="https://a0.muscache.com/im/pictures/5b4dc94a-0b4c-4c27-b50f-9c5a5b93c775.jpg?im_w=720"
-          title="Try hosting"
-          description="Earn extra income and unlock new opportunities by sharing your space."
-          buttonText="Learn more"
-        />
+        <section ref={observe}>
+          <LargeCard
+            img="https://a0.muscache.com/im/pictures/5b4dc94a-0b4c-4c27-b50f-9c5a5b93c775.jpg?im_w=720"
+            title="Try hosting"
+            description="Earn extra income and unlock new opportunities by sharing your space."
+            buttonText="Learn more"
+          />
+        </section>
       </main>
       <Footer />
     </div>
