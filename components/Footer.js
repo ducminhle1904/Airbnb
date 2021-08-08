6f7037475f34f0845be9bc821d0d850cfe514e22
Minh Đức <ducle7144@gmail.com>
2021-08-08 19:51:04 +0700

    first commit

---
+++ components/Footer.js
@@ -0,0 +1,51 @@
+function Footer() {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-4 px-32 py-14 bg-gray-100">
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">ABOUT</h5>
+        <p>How Airbnb works</p>
+        <p>Investors</p>
+        <p>HotelTonight</p>
+        <p>Careers</p>
+        <p>Newsroom</p>
+        <p>Airbnb Plus</p>
+        <p>Airbnb for Work</p>
+        <p>Founders' Letter</p>
+        <p>Airbnb 2021</p>
+        <p>Airbnb Luxe</p>
+        <p>Made possible by Hosts</p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">COMMUNITY</h5>
+        <p>Diversity & Belonging</p>
+        <p>Airbnb Associates</p>
+        <p>Gift cards</p>
+        <p>Against Discrimination</p>
+        <p>Frontline Stays</p>
+        <p>Airbnb.org</p>
+        <p>Accessibility</p>
+        <p>Guest Referrals</p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">HOST</h5>
+        <p>Host your home</p>
+        <p>Responsible hosting</p>
+        <p>Host an Online Experience</p>
+        <p>Resource Center</p>
+        <p>Host an Experience</p>
+        <p>Community Center</p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">SUPPORT</h5>
+        <p>Our COVID-19 Response</p>
+        <p>Neighborhood Support</p>
+        <p>Help Center</p>
+        <p>Trust & Safety</p>
+        <p>Cancellation options</p>
+      </div>
+      <div></div>
+    </div>
+  );
+}
+
+export default Footer;
--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,83 @@
-import Head from 'next/head'
+import Head from "next/head";
+import Image from "next/image";
+import Header from "../components/Header";
+import Banner from "../components/Banner";
+import SmallCard from "../components/SmallCard";
+import MediumCard from "../components/MediumCard";
+import LargeCard from "../components/LargeCard";
+import Footer from "../components/Footer";
 
-export default function Home() {
+export default function Home({ exploreData, cardData }) {
+  console.log(exploreData);
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="">
       <Head>
-        <title>Create Next App</title>
+        <title>AirBnb</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
+      <Header />
+      <Banner />
+      <main className="max-w-7xl mx-auto px-8 sm:px-16">
+        <section className="pt-6">
+          <h2 className="text-4xl font-semibold">Explore nearby</h2>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            {exploreData?.map((item, index) => (
+              <SmallCard
+                key={index}
+                img={item.img}
+                location={item.location}
+                distance={item.distance}
+              />
+            ))}
+          </div>
+        </section>
+        <section className="relative h-[200px] sm:h-[300px] lg:h-[400px] xl:h-[500px] 2xl:h-[600px] rounded-xl mt-5 overflow-hidden">
+          <Image
+            src="https://a0.muscache.com/im/pictures/a915ff27-6062-436d-a7a9-007685423f7b.jpg?im_w=720"
+            layout="fill"
+            objectFit="cover"
+          />
+          <div className="absolute top-[25%] w-full text-left pl-10">
+            <p className="text-sm sm:text-3xl font-semibold">
+              Not sure where <br /> to go? Perfect.
             </p>
-          </a>
+            <button className="bg-black text-white px-3 py-2 shadow-md rounded-xl font-bold my-3 hover:shadow-xl active:scale-90 transition duration-150 hover:bg-white hover:text-black">
+              I'm flexible{" "}
+            </button>
+          </div>
+        </section>
+        <section>
+          <h2 className="text-4xl font-semibold py-8">Live Anywhere</h2>
+          <div className="flex space-x-3 overflow-scroll scrollbar-hide p-3 -ml-3">
+            {cardData.map((item, index) => (
+              <MediumCard key={index} img={item.img} title={item.title} />
+            ))}
+          </div>
+        </section>
 
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <LargeCard
+          img="https://a0.muscache.com/im/pictures/5b4dc94a-0b4c-4c27-b50f-9c5a5b93c775.jpg?im_w=720"
+          title="Try hosting"
+          description="Earn extra income and unlock new opportunities by sharing your space."
+          buttonText="Learn more"
+        />
       </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
+      <Footer />
     </div>
-  )
+  );
+}
+
+export async function getStaticProps() {
+  const exploreData = await fetch("https://links.papareact.com/pyp").then(
+    (res) => res.json()
+  );
+  const cardData = await fetch("https://links.papareact.com/zp1").then((res) =>
+    res.json()
+  );
+  return {
+    props: {
+      exploreData,
+      cardData,
+    },
+  };
 }
